a9fba0d0ccbe1fa2002ddd5d21d1f16c29ea54dc
AyoubMokhtarii <ia_mokhtari@esi.dz>
2024-05-07 20:29:04 +0200

    faeture added Student

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-# hello 
+class Student :
+    def init():
+        pass
+
+    def get_student():
+        pass
+
+    def add_student():
+        pass
+
+    def remove_student():
+        pass
+
+    def update_student():
+        pass
