e9e4a0d39cbb994420ee5f7584ba80e167a1fdf5
AyoubMokhtarii <ia_mokhtari@esi.dz>
2024-05-07 20:30:21 +0200

    faeture added module

--- app.py
+++ app.py
@@ -13,3 +13,20 @@ class Student :
 
     def update_student():
         pass
+
+
+class Modules :
+    def init():
+        pass
+
+    def get_module():
+        pass
+
+    def add_module():
+        pass
+
+    def remove_module():
+        pass
+
+    def update_module():
+        pass
